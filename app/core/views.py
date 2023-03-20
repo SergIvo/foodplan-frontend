e91a07079ead5f878461c3be7d7144061aeda962
Sergey <Sergey@localhost.localdomain>
2023-03-20 18:32:07 +0300

    Switch to use models instead of API methods

--- app/core/views.py
+++ app/core/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 from core import api_methods
+from core.models import User, Recipe, Ingredient
 
 
 def index(request):
-    print(request.session.keys())
     context = {
-        'access_token': request.session.get('api_access_token')
+        'user_authorised': request.user.is_authenticated
     }
     return render(request, 'index.html', context)
 
 
 def register_user(request):
     if request.method == 'POST':
-        payload = {
-            'name': request.POST.get('name'),
-            'email': request.POST.get('email'),
-            'password': request.POST.get('password')
-        }
-        email, password = api_methods.create_user(payload)
-
-        user_token = api_methods.get_auth_token(
-            {
-                'email': email,
-                'password': password
-            }
+        user = User.objects.create_user(
+            email=request.POST.get('email'),
+            name=request.POST.get('name'),
+            password=request.POST.get('password')
         )
-        request.session['api_access_token'] = user_token
+
+        user = authenticate(email=email, password=password)
+
+        if user:
+            login(request, user)
 
         return redirect('index')
     context = {
@@ -36,62 +34,79 @@ def register_user(request):
 
 def authenticate_user(request):
     if request.method == 'POST':
-        payload = {
-            'email': request.POST.get('email'),
-            'password': request.POST.get('password')
-        }
-        user_token = api_methods.get_auth_token(payload)
-        request.session['api_access_token'] = user_token
+        user = authenticate(
+            email=request.POST.get('email'), 
+            password=request.POST.get('password')
+        )
 
-        return redirect('index')
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            alert_message = 'Email or password is incorrect'
     context = {
         'mock': 'mock'
     }
     return render(request, 'auth.html', context)
 
 
-def check_allergies(request_payload):
+def get_checked_allergies(request_payload):
     allergies = [
-        {'name': 'fish', 'label': 'Рыба', 'state': None},
-        {'name': 'meat', 'label': 'Мясо', 'state': None},
-        {'name': 'grain', 'label': 'Зерновые', 'state': None},
-        {'name': 'honey', 'label': 'Мед', 'state': None},
-        {'name': 'nuts', 'label': 'Орехи', 'state': None},
-        {'name': 'lactose', 'label': 'Лактоза', 'state': None}
+        'FISH',
+        'MEAT',
+        'GRAIN',
+        'HONEY',
+        'NUTS',
+        'LACTOSE',
     ]
-    for allergie in allergies:
-        allergie['state'] = request_payload.get(allergie['name'])
-    
+    checked_allergies = []
+    for allergy in allergies:
+        if request_payload.get(allergy):
+            checked_allergies.append(allergy)    
+    return checked_allergies
+
+
+def get_user_allergies(user):
+    allergies = [
+        {'name': 'FISH', 'label': 'Рыба', 'state': None},
+        {'name': 'MEAT', 'label': 'Мясо', 'state': None},
+        {'name': 'GRAIN', 'label': 'Зерновые', 'state': None},
+        {'name': 'HONEY', 'label': 'Мед', 'state': None},
+        {'name': 'NUTS', 'label': 'Орехи', 'state': None},
+        {'name': 'LACTOSE', 'label': 'Лактоза', 'state': None}
+    ]
+    for allergy in allergies:
+        if allergy['name'] in user.allergy_type:
+            allergy['state'] = 'checked'
     return allergies
 
 
 def user_account(request):
-    if not request.session.get('api_access_token'):
+    if not request.user.is_authenticated:
         return redirect('authenticate_user')
-    api_access_token = request.session.get('api_access_token')
-    user_email, user_name = api_methods.get_user_data(api_access_token)
     alert_message = None
     
     if request.method == 'POST':
-        payload = {
-            'email': request.POST.get('email'),
-            'password': request.POST.get('password'),
-            'name': request.POST.get('name')
-        }
-        if not payload['email']:
-            payload['email'] = user_email
-        if not payload['name']:
-            payload['name'] = user_name
-        if payload['password']:
-            user_email, user_name = api_methods.change_user_data(api_access_token, payload)
-            alert_message = 'User personal data changed'
-        else:
-            alert_message = 'Password is required for changing user data'
-    
-    allergies = check_allergies(request.POST)
+        if request.POST.get('email'):
+            request.user.email = request.POST.get('email')
+        if request.POST.get('name'):
+            request.user.name = request.POST.get('name')
+        if request.POST.get('password'):
+            request.user.set_password(request.POST.get('password'))
+        
+        checked_allergies = get_checked_allergies(request.POST)
+        if not set(request.user.allergy_type) == set(checked_allergies):
+            request.user.allergy_type = checked_allergies
+
+        request.user.save()
+        alert_message = 'User personal data changed'
+
+    user_email = request.user.email
+    user_name = request.user.name
+    allergies = get_user_allergies(request.user)
     
     context = {
-        'user': {'name': user_name, 'email': user_email, 'password': 'some_password'},
+        'user': {'name': user_name, 'email': user_email, 'password': '*********'},
         'allergies': allergies,
         'menu': {'name': 'menu1', 'calories': 100500, 'persons': 2, 'allergies': 'something', 'eating_times': 4},
         'alert': alert_message
@@ -100,23 +115,27 @@ def user_account(request):
 
 
 def logout_user(request):
-    if not request.session.get('api_access_token'):
+    if not request.user.is_authenticated:
         return redirect('authenticate_user')
-    request.session.pop('api_access_token')
+    logout(request)
     return redirect('index')
 
 
 def show_recipes_cards(request):
-    if not request.session.get('api_access_token'):
+    if not request.user.is_authenticated:
         return redirect('authenticate_user')
-        
-    api_access_token = request.session.get('api_access_token')
-    recipes = api_methods.get_recipes(api_access_token)
+
+    recipes = list(Recipe.objects.all().prefetch_related('ingredients'))
     for recipe in recipes:
         print('Recipe:', recipe)
-        total_calories = sum([ingredient['calories'] for ingredient in recipe['ingredients']])
-        recipe['total_calories'] = total_calories
+        recipe.ingreds = recipe.ingredients.all()
+        total_calories = sum([ingredient.calories for ingredient in recipe.ingreds])
+        recipe.total_calories = total_calories
+        recipe.fits = True
+        if set(recipe.allergy_type).intersection(set(request.user.allergy_type)):
+            recipe.fits = False
     
+    recipes = [recipe for recipe in recipes if recipe.fits]
     context = {
         'recipes': recipes
     }
